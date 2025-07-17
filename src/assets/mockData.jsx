b20f0c905cd20a2bff5b638badcdfcee3f88ccab
olaniyan oluwagbolahan <olaniyangbolahan01@gmail.com>
2025-07-17 12:07:35 +0100

    Initial commit

---
+++ src/assets/mockData.jsx
@@ -0,0 +1,85 @@
+import Image1 from './Images/Bag.jpg'
+import Image2 from './Images/Boot.jpg'
+import Image3 from './Images/Cap.jpg'
+import Image4 from './Images/Dove.jpg'
+import Image5 from './Images/Headset.jpg'
+import Image6 from './Images/Robot.jpg'
+import Image7 from './Images/Sandals.jpg'
+import Image8 from './Images/Shirts.jpg'
+import Image9 from './Images/Watch1.jpg'
+import Image10 from './Images/Watch2.jpg'
+
+
+export const Categories = [
+    "Electronics",
+    "Fashion",
+    "Home & Kitchen",
+    "Beauty",
+    "Sports",
+    "Automotive",
+];
+
+export const mockData = [
+    {
+        id: 1,
+        image: Image9,
+        name: 'product 1',
+        price: 29.99,
+    },
+    {
+        id: 2,
+        image: Image8,
+        name: 'product 2',
+        price: 39.99,
+    },
+    {
+        id: 3,
+        image: Image7,
+        name: 'product 3',
+        price: 39.99, 
+    },
+     {
+        id: 4,
+        image: Image6,
+        name: 'product 4',
+        price: 49.99,
+     },
+     {
+        id: 5,
+        image: Image10,
+        name: 'product 5',
+        price: 49.99,
+     },
+     {
+        id: 6,
+        image: Image4,
+        name: 'product 6',
+        price: 49.99,
+     },
+     {
+        id: 7,
+        image: Image5,
+        name: 'product 7',
+        price: 49.99,
+     },
+     {
+         id: 8,
+        image: Image3,
+        name: 'product 8',
+        price: 49.99,
+     },
+      {
+        id: 9,
+        image: Image2,
+        name: 'product 9',
+        price: 49.99,
+      },
+      {
+        id: 10,
+        image: Image1,
+        name: 'product 10',
+        price: 49.99,
+      },
+      
+];
+
