43403cb334b782325ed9cf7c35ac1714f9a3dc2a
olaniyan oluwagbolahan <olaniyangbolahan01@gmail.com>
2025-08-18 23:14:07 +0100

    fixed the emptyCart syntax

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import EmptyCart from '../assets/Images/EmptyCart.jpg'
+import EmptyCart from '../assets/Images/emptyCart.jpg'
 import { FaTrashAlt } from 'react-icons/fa'
 import Modal from '../components/Modal'
 import ChangeAddress from '../components/ChangeAddress'
